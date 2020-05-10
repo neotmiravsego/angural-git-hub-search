b81e48ce6445be26d8ecdb64880b29ba12eaa633
Gregory <neotmiravsego@gmail.com>
2020-05-11 00:12:40 +0200

    added validators input and  input button search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +12,14 @@ import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RepositoryComponent],
-  imports: [CommonModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [HttpService],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { repoInfo } from '../repository/IRepoInfo';
 import { repositoriesInfo } from '../repository/IreposInfo';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -13,21 +14,27 @@ export class HomeComponent implements OnInit {
   repositories: repositoriesInfo[] = [];
   repo: repoInfo;
   isSortedSize: boolean = false;
-  isSortedWatch: boolean=false;
+  isSortedWatch: boolean = false;
+  searchText: string;
+  form: FormGroup = new FormGroup({
+    searchInfo: new FormControl('', Validators.required),
+  });
   constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {}
 
-  searchRepositories(c: string) {
-    this.httpService.getRepositories(c).subscribe((data) => {
+  searchRepositories(searchText) {
+    this.httpService.getRepositories(searchText).subscribe((data) => {
       this.repositories = data;
     });
   }
   sortSize() {
     if (!this.isSortedSize) {
-      this.repositories = this.repositories.sort((a: repositoriesInfo, b: repositoriesInfo) => {
-        return a.size - b.size;
-      });
+      this.repositories = this.repositories.sort(
+        (a: repositoriesInfo, b: repositoriesInfo) => {
+          return a.size - b.size;
+        }
+      );
       this.isSortedSize = true;
     } else {
       this.repositories = this.repositories.reverse();
@@ -35,9 +42,11 @@ export class HomeComponent implements OnInit {
   }
   sortWatchers() {
     if (!this.isSortedWatch) {
-      this.repositories = this.repositories.sort((a: repositoriesInfo, b: repositoriesInfo) => {
-        return a.watchers - b.watchers;
-      });
+      this.repositories = this.repositories.sort(
+        (a: repositoriesInfo, b: repositoriesInfo) => {
+          return a.watchers - b.watchers;
+        }
+      );
       this.isSortedWatch = true;
     } else {
       this.repositories = this.repositories.reverse();
